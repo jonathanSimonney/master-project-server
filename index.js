db6e49bbe38477574255fbaf4951e67a803d07dc
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-11-02 13:54:45 +0100

    file now saved on server, download instruction stille to be sent.

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
+const fs = require('fs');  // file system
 const PORT = 3000
 const HOST = 'localhost'
-
+const youtubeStream = require('youtube-audio-stream')
 // app.use(express.static('public'))
+musicId = 0
 
 app.get('/api/dl/:youtubeUrl', (req, res) => {
-    //todo
+    youtubeUrl = req.params.youtubeUrl;
+    try {
+        const musicFile = 'temp/music' + musicId + '.mp3';
+        fs.closeSync(fs.openSync(musicFile, 'w'));
+        const writeStream = fs.createWriteStream(musicFile);
+        youtubeStream(youtubeUrl).pipe(writeStream)
+        musicId++
+        res.send("should be a download instruction...")
+    } catch (exception) {
+        console.log(exception)
+        res.status(500).send(exception)
+    }
 })
 
 
