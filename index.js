03950771bc58fe7a843a1c3cff5403c656fce213
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-12-14 09:10:23 +0100

    download with fileAuthor name functional.

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const express = require('express')
 const app = express()
-const fs = require('fs');  // file system
+const fs = require('fs-extra');  // file system
+const NodeID3 = require('node-id3')
 const PORT = process.env.PORT || 3000
 const HOST = 'localhost'
 const youtubeStream = require('youtube-audio-stream')
 app.use(express.static('temp'))
-musicId = 0
+let musicId = 0
 
-app.get('/api/dl/:youtubeUrl/:filename',  (req, res) => {
+app.get('/api/dl/:youtubeUrl/:filename/:fileAuthor',  (req, res) => {
+    musicId ++;
+    let toBeDownloadedFilePath = "musics/music" + musicId + ".mp3";
     const youtubeUrl = req.params.youtubeUrl;
     const filename = req.params.filename;
-    try {
-        youtubeStream(youtubeUrl).pipe(res)
+    const fileAuthor = req.params.fileAuthor;
 
-        res.setHeader('Content-disposition', 'attachment; filename=' + filename + ".mp3");
-        res.setHeader('Content-type', 'mp3');
+    fs.ensureFileSync(toBeDownloadedFilePath);
+
+    const toBeDownloadedFile = fs.createWriteStream(toBeDownloadedFilePath);
+    try {
+        let fileWriteStream = youtubeStream("https://youtu.be/Fna56a_r41s").pipe(toBeDownloadedFile)
+
+        fileWriteStream.on('finish', () => {
+            let tags = {
+                title: filename,
+                artist: fileAuthor,
+            }
+
+            NodeID3.update(tags, toBeDownloadedFilePath)
+
+            res.download(toBeDownloadedFilePath)
+            res.setHeader('Content-disposition', 'attachment; filename=' + filename + ".mp3");
+        })
     } catch (exception) {
         console.log(exception)
         res.status(500).send(exception)
