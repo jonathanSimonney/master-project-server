bc88d96ecf416743e6abc5f6cb866aebbbf6b850
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-12-17 09:45:34 +0100

    author added only with streams.

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 const express = require('express')
 const app = express()
-const fs = require('fs-extra');  // file system
+// const fs = require('fs-extra');  // file system
 const NodeID3 = require('node-id3')
 const PORT = process.env.PORT || 3000
 const HOST = 'localhost'
+const stream = require('stream');
+const CombinedStream = require('combined-stream');
 const youtubeStream = require('youtube-audio-stream')
 app.use(express.static('temp'))
 let musicId = 0
 
 app.get('/api/dl/:youtubeUrl/:filename/:fileAuthor',  (req, res) => {
     musicId ++;
-    let toBeDownloadedFilePath = "musics/music" + musicId + ".mp3";
+    // let toBeDownloadedFilePath = "musics/music" + musicId + ".mp3";
+    // const youtubeUrl = "https://youtu.be/JU5LMG3WFBw"
     const youtubeUrl = req.params.youtubeUrl;
     const filename = req.params.filename;
     const fileAuthor = req.params.fileAuthor;
 
-    fs.ensureFileSync(toBeDownloadedFilePath);
+    // fs.ensureFileSync(toBeDownloadedFilePath);
 
-    const toBeDownloadedFile = fs.createWriteStream(toBeDownloadedFilePath);
+    // const toBeDownloadedFile = fs.createWriteStream(toBeDownloadedFilePath);
     try {
-        let fileWriteStream = youtubeStream(youtubeUrl).pipe(toBeDownloadedFile)
+        let tags = {
+            title: filename,
+            artist: fileAuthor,
+        }
 
-        fileWriteStream.on('finish', () => {
-            let tags = {
-                title: filename,
-                artist: fileAuthor,
-            }
+        let ID3FrameBuffer = NodeID3.create(tags)
 
-            NodeID3.update(tags, toBeDownloadedFilePath)
+        let bufferStream = new stream.PassThrough();
+        bufferStream.end( ID3FrameBuffer );
 
-            res.download(toBeDownloadedFilePath)
-            res.setHeader('Content-disposition', 'attachment; filename=' + filename + ".mp3");
-        })
+        let combinedStream = CombinedStream.create();
+
+        combinedStream.append(bufferStream)
+        combinedStream.append(youtubeStream(youtubeUrl))
+
+        combinedStream.pipe(res)
+
+        res.setHeader('Content-disposition', 'attachment; filename=' + filename + ".mp3");
+        res.setHeader('Content-type', 'mp3');
     } catch (exception) {
         console.log(exception)
         res.status(500).send(exception)
